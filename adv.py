edcf872af94e9a1a1cf53f8d1c6363183e96152c
janaksuthar <42261530+janaksuthar@users.noreply.github.com>
2025-11-21 21:57:51 +0530

    Fix indentation and correct success message typo

--- adv.py
+++ adv.py
@@ -20,5 +20,6 @@ Newspaper=st.number_input("Newspaper Adv budget", min_value=0.0)
 if st.button('predict sales'):
 	input_data=np.array([[TV, Radio,Newspaper]])
 	prediction=model.predict(input_data)[0]
-	st.sucess(f'predict sales:{prediction:.2f}')
+	st.success(f'predict sales:{prediction:.2f}')
+
 
