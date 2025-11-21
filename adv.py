d9b41ae4bf0bc3d055a009b86dfd2bfb785343b7
janaksuthar <42261530+janaksuthar@users.noreply.github.com>
2025-11-21 21:54:45 +0530

    Correct imports and fix prediction output function
    Fix typo in 'streamlit' and correct function name from 'sucess' to 'success'.

--- adv.py
+++ adv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import joblib
-import stremlit as st
+import streamlit as st
 
 #Load the ML model
 
@@ -20,4 +20,5 @@ Newspaper=st.number_input("Newspaper Adv budget", min_value=0.0)
 if st.button('predict sales'):
 	input_data=np.array([[TV, Radio,Newspaper]])
 	prediction=model.predict(input_data)[0]
+
 	st.sucess(f'predict sales:{prediction:.2f}')
